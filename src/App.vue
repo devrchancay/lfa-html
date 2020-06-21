743fb2cfe301d2ca377157efc9aab00b0fad7cf0
Ramon Chancay <ramon.chancay@gmail.com>
2020-06-20 22:37:14 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <div id="app">
+  <div class="container">
     <Navbar />
+    <Hero />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Hero
   }
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+
+html,
+body {
+  font-family: 'Roboto Condensed', sans-serif;
+}
+
+#app {
+  font-family: 'Roboto Condensed', sans-serif;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full">
+    <div class="flex py-2 px-2">
+      <div class="w-1/2">
+        <span class="bg-blue-700 p-1 text-white text-center text-xs">ADIDAS</span>
+        <h1 class="text-3xl leading-tight my-2">Adidas predator x232 running</h1>
+        <button class="bg-blue-500 px-4 py-1 rounded-md text-white block">COMPRAR</button>
+        <a href="/">CATÁLOGO</a>
+      </div>
+      <div class="w-1/2">
+        <img src="../assets/zapato.svg" />
+      </div>
+    </div>
+  </div>
+</template>
